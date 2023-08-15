64943c7266100cd259f1430fc069ce255555ff9d
aryanteng <aryanteng892@gmail.com>
2023-08-15 23:45:15 +0530

    search functionality completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,24 @@ import { WeatherData } from './models/weather.model';
 })
 export class AppComponent implements OnInit {
   weatherData: WeatherData;
+  city: string;
   constructor(private weatherService: WeatherService) {}
   ngOnInit(): void {
     this.weatherService.getWeatherData('New Delhi').subscribe({
       next: (data) => {
         console.log(data);
         this.weatherData = data;
+      },
+    });
+  }
+  onSubmit() {
+    this.weatherService.getWeatherData(this.city).subscribe({
+      next: (data) => {
+        console.log('SEARCH', data);
+        this.weatherData = data;
+      },
+      error: (error) => {
+        alert(error.error.error.message);
       },
     });
   }
